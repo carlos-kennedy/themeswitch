7b12e97f2d0b2eccc93adbfa2ef83e8f9074aaa2
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-09-06 20:14:11 -0300

    Implementando texto e alguns estilos novos.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const elBody = document.querySelector("body");
+const elH1 = document.querySelector("h1");
 const elSwitch = document.querySelector(".switch");
 const switchSound = new Audio("./assets/switchSound.mp3");
 
 const changeThemeSwitch = () => {
   elBody.classList.toggle("dark");
   switchSound.play();
+
+  if (elH1.innerText === "LIGHT") {
+    elH1.innerText = "Dark";
+  } else {
+    elH1.innerText = "Light";
+  }
 };
 
 elSwitch.addEventListener("click", changeThemeSwitch);
